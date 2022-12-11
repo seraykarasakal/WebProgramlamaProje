8464185b4a73f836fd5544c7ce6d5e8942f50858
seraykarasakal <seraykrskl@gmail.com>
2022-12-11 23:04:43 +0300

    Katmanlar oluşturuldu

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    internal class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogImage { get; set; }
+        public string BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    internal class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentUserName { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+
+        public bool CommentStatus { get; set; }
+    }
+}
