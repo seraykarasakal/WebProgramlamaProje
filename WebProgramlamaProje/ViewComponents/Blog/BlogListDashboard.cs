201349c0f3c4632106431ee0443d53369388c9d6
seraykarasakal <seraykrskl@gmail.com>
2022-12-19 14:55:41 +0300

    Puanlama ve Total Rayting Databasede oluşturuldu

---
+++ WebProgramlamaProje/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebProgramlamaProje.ViewComponents.Blog
+{
+    public class BlogListDashboard :ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
