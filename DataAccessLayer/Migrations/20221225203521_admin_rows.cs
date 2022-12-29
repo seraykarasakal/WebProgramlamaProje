86f4c7d524606d91c579bac2a84d5de7cdbc9f55
seraykarasakal <seraykrskl@gmail.com>
2022-12-30 01:43:25 +0300

    Admin paneli oluşturuldu

---
+++ DataAccessLayer/Migrations/20221225203521_admin_rows.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class adminrows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdminUserName",
+                table: "Admins",
+                newName: "AdminMail");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdminMail",
+                table: "Admins",
+                newName: "AdminUserName");
+        }
+    }
+}
